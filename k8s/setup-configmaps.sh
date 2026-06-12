50f67c36c3fb6be842823ac47e23f0977edb7deb
dpduy123 <dpduy1230901tl@gmail.com>
2026-06-12 11:20:38 +0700

    feat: implement k8s

---
+++ k8s/setup-configmaps.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# Script để tạo các K8s ConfigMaps từ các file cấu hình nằm trong thư mục deployments/monitoring
+# Bạn nên chạy script này trước khi apply các file yaml (kubectl apply -f k8s/)
+
+echo "Tạo ConfigMaps cho Prometheus..."
+kubectl create configmap prometheus-config --from-file=../deployments/monitoring/prometheus/prometheus.yml -o yaml --dry-run=client | kubectl apply -f -
+kubectl create configmap prometheus-rules --from-file=../deployments/monitoring/prometheus/rules -o yaml --dry-run=client | kubectl apply -f -
+
+echo "Tạo ConfigMaps cho Loki..."
+kubectl create configmap loki-config --from-file=../deployments/monitoring/loki/loki-config.yaml -o yaml --dry-run=client | kubectl apply -f -
+
+echo "Tạo ConfigMaps cho Promtail..."
+kubectl create configmap promtail-config --from-file=../deployments/monitoring/promtail/promtail-config.yaml -o yaml --dry-run=client | kubectl apply -f -
+
+echo "Tạo ConfigMaps cho Grafana..."
+kubectl create configmap grafana-provisioning --from-file=../deployments/monitoring/grafana/provisioning -o yaml --dry-run=client | kubectl apply -f -
+kubectl create configmap grafana-dashboards --from-file=../deployments/monitoring/grafana/dashboards -o yaml --dry-run=client | kubectl apply -f -
+
+echo "Tạo ConfigMaps thành công!"
